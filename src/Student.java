5c0105ba9eadd191b8f1d1d50f3d0262f73a5a0b
Lcarrot <vip.kfkf2013@bk.ru>
2020-03-24 17:33:28 +0300

    forrgot Student

---
+++ src/Student.java
@@ -0,0 +1,44 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    private String name;
+    private String surname;
+    private boolean sex;
+    private int group;
+
+    public Student(String name, String surname, boolean sex, int group) {
+        this.name = name;
+        this.surname = surname;
+        this.sex = sex;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public boolean getSex() {
+        return sex;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+
+    @Override
+    public String toString() {
+        String gender =  sex ? "man" : "woman";
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", group=" + group +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
